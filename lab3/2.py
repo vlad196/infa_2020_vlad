7d0080ac8870011d9f7648f07339b9ea8f79a7fc
vlad196 <65094576+vlad196@users.noreply.github.com>
2020-05-20 00:20:47 +0200

    add second image

---
+++ lab3/2.py
@@ -0,0 +1,18 @@
+from graph import *
+
+brushColor("yellow")
+circle(250,250,200)
+brushColor("red")
+circle(150,210,50)
+brushColor("red")
+circle(350,210,40)
+brushColor("black")
+circle(150,210,30)
+brushColor("black")
+circle(350,210,25)
+a=[[55,100],[45,110],[190,210],[205,200]]
+polygon(a)
+b=[[400,100],[405,110],[300,210],[280,200]]
+polygon(b)
+rectangle(190,350,350,380)
+run()
